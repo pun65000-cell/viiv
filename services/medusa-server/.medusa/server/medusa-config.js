34dcbc9a3fac57e25c58f72263147c7bb432f277
Punnanat Kai <pun65000@gmail.com>
2026-03-16 13:59:30 +0000

    VIIV kernel runtime fully restored

---
+++ services/medusa-server/.medusa/server/medusa-config.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("@medusajs/framework/utils");
+(0, utils_1.loadEnv)(process.env.NODE_ENV || "development", process.cwd());
+module.exports = (0, utils_1.defineConfig)({
+    projectConfig: {
+        databaseUrl: process.env.DATABASE_URL,
+        http: {
+            storeCors: process.env.STORE_CORS,
+            adminCors: process.env.ADMIN_CORS,
+            authCors: process.env.AUTH_CORS,
+            jwtSecret: process.env.JWT_SECRET,
+            cookieSecret: process.env.COOKIE_SECRET,
+        },
+    },
+    admin: {
+        disable: false
+    }
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVkdXNhLWNvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL21lZHVzYS1jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxxREFBaUU7QUFFakUsSUFBQSxlQUFPLEVBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLElBQUksYUFBYSxFQUFFLE9BQU8sQ0FBQyxHQUFHLEVBQUUsQ0FBQyxDQUFBO0FBRTdELE1BQU0sQ0FBQyxPQUFPLEdBQUcsSUFBQSxvQkFBWSxFQUFDO0lBQzVCLGFBQWEsRUFBRTtRQUNiLFdBQVcsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFlBQWE7UUFDdEMsSUFBSSxFQUFFO1lBQ0osU0FBUyxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsVUFBVztZQUNsQyxTQUFTLEVBQUUsT0FBTyxDQUFDLEdBQUcsQ0FBQyxVQUFXO1lBQ2xDLFFBQVEsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFNBQVU7WUFDaEMsU0FBUyxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsVUFBVztZQUNsQyxZQUFZLEVBQUUsT0FBTyxDQUFDLEdBQUcsQ0FBQyxhQUFjO1NBQ3pDO0tBQ0Y7SUFFRCxLQUFLLEVBQUU7UUFDTCxPQUFPLEVBQUUsS0FBSztLQUNmO0NBQ0YsQ0FBQyxDQUFBIn0=
---
+++ services/medusa-server/.medusa/server/public/admin/assets/chunk-F6ZOHZVB-DkGB9kE-.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DwV1e92l.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
